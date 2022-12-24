dad400395911d0ba800b5df4c871bd755f88cc47
OleksandrDika <109437683+OleksandrDika@users.noreply.github.com>
2022-12-24 15:50:41 +0200

    make task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
 
+const btnStart = document.querySelector("button[data-start]");
+const btnStop = document.querySelector("button[data-stop]");
+
+btnStart.addEventListener("click", getRandomHexColor);
+btnStop.addEventListener("click", onStop);
+
+btnStop.setAttribute('disabled', true) 
+
+function getRandomHexColor() {
+    timerId = setInterval(() => {
+        return document.body.style.backgroundColor =  `#${Math.floor(Math.random() * 16777215).toString(16)}`;;
+      }, 1000);  
+      btnStart.setAttribute('disabled', true)  
+      btnStop.removeAttribute('disabled')        
+};
+
+function onStop (even) {
+    clearInterval(timerId);    
+    btnStart.removeAttribute('disabled') 
+    btnStop.setAttribute('disabled', true) 
+}
+
+
+
