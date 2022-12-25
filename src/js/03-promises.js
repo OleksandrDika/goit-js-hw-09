deafeb768c8eb3dd3e78245bab7a68034a63d49a
OleksandrDika <109437683+OleksandrDika@users.noreply.github.com>
2022-12-25 18:38:59 +0200

    make Task3 HW9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const btnSub = document.querySelector('button[type=submit]')
+
+form.addEventListener('input', onInput)
+btnSub.addEventListener('click', onSubmit)
+
+function onInput(even) {
+  const firstDelay = form.delay.value;
+  const delayStep = form.step.value;
+  const amount = form.amount.value;  
+  return result = {firstDelay, delayStep, amount}  
+}
+
+function onSubmit(even) {
+  btnSub.setAttribute('type', 'button'); 
+  setTimeout(() => {
+    
+    for (let index = 1; index <= result.amount; index++) {
+      
+        createPromise(index, result.delayStep);     
+      
+    } 
+  }, result.firstDelay);
+   
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
-}
+};
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//     // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//     // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
